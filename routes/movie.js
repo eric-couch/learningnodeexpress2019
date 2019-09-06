0be435897df6603b10e2a8d625c1a9c24f81d606
eric-couch <James.Couch@edgetechacademy.edu>
2019-09-05 21:05:12 -0500

    added get List By Form

--- routes/movie.js
+++ routes/movie.js
@@ -17,7 +17,6 @@ router.all('/getMovie', function (req, res, next) {
         name = req.query.name;
     }
 
-
     let fetchURL = 'http://www.omdbapi.com/?apikey=86c39163&t=' + name;
     console.log(fetchURL);
     fetch(fetchURL)
@@ -31,6 +30,38 @@ router.all('/getMovie', function (req, res, next) {
             res.send(moviePage);
         });
 })
+
+router.post('/getMovieList', function (req, res, next) {
+    let name = req.body.name;
+    let fetchURL = 'http://www.omdbapi.com/?apikey=86c39163&s=' + name;
+    console.log(fetchURL);
+    fetch(fetchURL)
+        .then(r => r.json())
+        .then(body => {
+            let returnHTML = "";
+            console.log(body);
+            body.Search.forEach(function (item) {
+                returnHTML += "<a href='/movie/getMovieByIMDB?i=" + item.imdbID + "'>" + item.Title + "<br>";
+            })
+            res.send(returnHTML);
+        })
+});
+
+router.get('/getMovieByIMDB', function (req, res, next) {
+    let imdb = req.query.i;
+    let fetchURL = 'http://www.omdbapi.com/?apikey=86c39163&i=' + imdb;
+    console.log(fetchURL);
+    fetch(fetchURL)
+        .then(r => r.json())
+        .then(body => {
+            let moviePage = "<div style='text-align:center;width:400px'><h1>" + body.Title +
+                "</h1><br>";
+            //moviePage += '<br />' + body.Actors.replace(/,/g, " | ");
+            moviePage += '<br><h2>Release Date: ' + body.Released + '</h2>'
+            moviePage += "<br><img src='" + body.Poster + "' width=300></div>"
+            res.send(moviePage);
+        })
+});
 
 router.get('/searchMovie', function (req, res, next) {
     let name = req.query.name;
