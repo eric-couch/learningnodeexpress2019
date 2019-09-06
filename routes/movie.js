fc94f70e34462ed8db67721fb8f199107d573ac6
eric-couch <James.Couch@edgetechacademy.edu>
2019-09-05 19:00:01 -0500

    add form post

--- routes/movie.js
+++ routes/movie.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 let fetch = require('node-fetch');
 
+let moviePage = [];
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', { title: 'Happy Birthday' });
@@ -15,27 +17,46 @@ router.get('/getMovie', function (req, res, next) {
     fetch(fetchURL)
         .then(r => r.json())
         .then(body => {
-            let moviePage = "<h1>" + body.Title +
-                "</h1><br><img src='" + body.Poster + "'>";
+            let moviePage = "<div style='text-align:center;width:400px'><h1>" + body.Title +
+                "</h1><br>";
+            //moviePage += '<br />' + body.Actors.replace(/,/g, " | ");
+            moviePage += '<br><h2>Release Date: ' + body.Released + '</h2>'
+            moviePage += "<br><img src='" + body.Poster + "' width=300></div>"
             res.send(moviePage);
         });
 })
 
 router.get('/searchMovie', function (req, res, next) {
     let name = req.query.name;
-
     let fetchURL = 'http://www.omdbapi.com/?apikey=86c39163&s=' + name;
     console.log(fetchURL);
     fetch(fetchURL)
         .then(r => r.json())
         .then(body => {
-            let moviePage = "";
             body.Search.forEach(function (item) {
-                moviePage += "<h1>" + item.Title +
-                    "</h1><br><img src='" + item.Poster + "'>";
+                moviePage.push({ Title: item.Title, Poster: item.Poster, IMDB: item.imdbID });
             })
-            res.send(moviePage)
+            return Promise.resolve(moviePage);
+        })
+        .then(body => {
+            //let movies = [];
+            body.forEach(function (item) {
+                let detailURL = 'http://www.omdbapi.com/?apikey=86c39163&i=' + item.IMDB;
+                console.log(detailURL);
+                fetch(detailURL)
+                    .then(r => r.json())
+                    .then(body => {
+                        // let movies = "";
+                        // movies += body.Title + '<br>' + body.Actors + '<img src="' + body.Poster + '">';
+                        //item.Actors = body.Actors;
+                        moviePage.push({ Title: body.Title, Poster: body.Poster, Actors: body.Actors });
+                    })
+                return Promise.resolve(moviePage);
+                // .then(body => res.send(body));
+            })
+            res.send(body)
         });
+
 });
 
 
