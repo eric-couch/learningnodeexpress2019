a252810226fb2530db438dc2b8ca3dfb7a529e9c
eric-couch <James.Couch@edgetechacademy.edu>
2019-09-06 21:14:15 -0500

    added searchMovie to return two sets

--- routes/movie.js
+++ routes/movie.js
@@ -56,7 +56,9 @@ router.get('/getMovieByIMDB', function (req, res, next) {
         .then(body => {
             let moviePage = "<div style='text-align:center;width:400px'><h1>" + body.Title +
                 "</h1><br>";
-            //moviePage += '<br />' + body.Actors.replace(/,/g, " | ");
+            if (body.Actors.length > 0) {
+                moviePage += '<br />' + body.Actors.replace(/,/g, " | ");
+            }
             moviePage += '<br><h2>Release Date: ' + body.Released + '</h2>'
             moviePage += "<br><img src='" + body.Poster + "' width=300></div>"
             res.send(moviePage);
@@ -66,35 +68,34 @@ router.get('/getMovieByIMDB', function (req, res, next) {
 router.get('/searchMovie', function (req, res, next) {
     let name = req.query.name;
     let fetchURL = 'http://www.omdbapi.com/?apikey=86c39163&s=' + name;
+    let moviePage = [];
     console.log(fetchURL);
     fetch(fetchURL)
         .then(r => r.json())
         .then(body => {
+            console.log(body);
             body.Search.forEach(function (item) {
                 moviePage.push({ Title: item.Title, Poster: item.Poster, IMDB: item.imdbID });
             })
             return Promise.resolve(moviePage);
         })
         .then(body => {
-            //let movies = [];
-            body.forEach(function (item) {
-                let detailURL = 'http://www.omdbapi.com/?apikey=86c39163&i=' + item.IMDB;
-                console.log(detailURL);
-                fetch(detailURL)
-                    .then(r => r.json())
-                    .then(body => {
-                        // let movies = "";
-                        // movies += body.Title + '<br>' + body.Actors + '<img src="' + body.Poster + '">';
-                        //item.Actors = body.Actors;
-                        moviePage.push({ Title: body.Title, Poster: body.Poster, Actors: body.Actors });
+            fetchURL = 'http://www.omdbapi.com/?apikey=86c39163&s=' + name + '&page=2'
+            fetch(fetchURL)
+                .then(r => r.json())
+                .then(body => {
+                    console.log(body);
+                    body.Search.forEach(function (item) {
+                        moviePage.push({ Title: item.Title, Poster: item.Poster, IMDB: item.imdbID });
                     })
-                return Promise.resolve(moviePage);
-                // .then(body => res.send(body));
-            })
-            res.send(body)
-        });
+                    res.send(moviePage);
+                })
+
+        })
 
 });
+
+
 
 
 module.exports = router;
